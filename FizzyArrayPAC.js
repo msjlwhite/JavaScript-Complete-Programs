f5a320563984961c1593a0bae197bc59616a371d
msjlwhite <msjlwhite@gmail.com>
2018-12-10 10:43:47 -0600

    JavaScript Examples 12.20.2018

---
+++ FizzyArrayPAC.js
@@ -0,0 +1,18 @@
+// J White
+// 12/10/2018
+// creates a function to 
+//
+
+
+function numarray(n) {
+    resultArr = [n];
+    for (i = 0; i < n; i++) {
+        resultArr[i] = i ;
+    }
+    console.log(resultArr);
+}
+
+
+// this starts the method to load numbers/value into the array
+numarray(7);
+numarray(4);
---
+++ FizzyArrayTPR.js
@@ -0,0 +1,15 @@
+// J White
+// 12.10.2018
+
+
+function numarray(n) {
+    resultArr = [n];
+    for (i = 0; i < n; i++) {
+        resultArr[i] = i ;
+    }
+    console.log(resultArr);
+}
+
+//
+numarray(7);
+numarray(4);
